19840f7f407975219bfcb6d5b7209ff9687e70e6
김경록 <goodboy3421@gmail.com>
2020-05-28 16:15:54 +0900

    initial commit

---
+++ src/main/java/com/example/toyproject/ToyprojectApplication.java
@@ -0,0 +1,11 @@
+package com.example.toyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyprojectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ToyprojectApplication.class, args);
+    }
+}
